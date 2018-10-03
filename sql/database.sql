43d4ffd7d74ccf15354093ec1159f0f780253166
vs-slavchev <veselin.slavchev@student.fontys.nl>
2018-10-03 22:50:11 +0200

    Comment out play container and make sql directory

---
+++ sql/database.sql
@@ -0,0 +1,25 @@
+CREATE TABLE app_user (
+user_id varchar(64) primary key,
+name varchar(64) NOT NULL
+);
+
+CREATE TABLE tracked_activity (
+tracked_activity_id int8 primary key,
+activity_blueprint_id int8 references activity_blueprint(activity_blueprint_id) ON DELETE NO ACTION ON UPDATE NO ACTION,
+user_id varchar(64) references app_user(user_id) ON DELETE NO ACTION ON UPDATE NO ACTION,
+time_start timestamp NOT NULL,
+duration_minutes int4 CHECK (duration_minutes >= 0),
+public_visibility boolean DEFAULT false
+);
+
+CREATE TABLE activity_blueprint (
+activity_blueprint_id int8 primary key,
+name varchar(32) NOT NULL UNIQUE
+);
+
+CREATE TABLE friendship (
+friendship_id int8 primary key,
+inviter_user_id varchar(64) references app_user(user_id) ON DELETE NO ACTION ON UPDATE NO ACTION,
+invitee_user_id varchar(64) references app_user(user_id) ON DELETE NO ACTION ON UPDATE NO ACTION,
+accepted boolean DEFAULT false
+);
