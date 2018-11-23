966adc9bd6d6908a7da9ef27a37a06a6b1f2939a
vesnenokanna <a.vesnenok@student.fontys.nl>
2018-11-23 16:38:02 +0100

    Statistics page added including a pie chart

--- ns-angular-mobile/src/app/app-settings.ts
+++ ns-angular-mobile/src/app/app-settings.ts
@@ -1,4 +1,5 @@
 export class AppSettings {
     public static API_URL = 'http://62.108.10.166:9000';
     public static USER_ID = 1;
+    public static showBottomBar = true;
 }
--- ns-angular-mobile/src/app/statistic/statistics.component.ts
+++ ns-angular-mobile/src/app/statistic/statistics.component.ts
@@ -2,14 +2,27 @@ import { Component, OnInit } from "@angular/core";
 
 import { Statistic } from "./statistic";
 import { StatisticService } from "./statistic.service";
+import { AppSettings } from "~/app/app-settings";
+import * as chartModule from "nativescript-ui-chart";
 
 @Component({
     selector: "ns-statistics",
     moduleId: module.id,
     templateUrl: "./statistics.component.html",
+    styleUrls: ["./statistic-style.css"],
+    // template: `<ActionBar title="Application Title"></ActionBar>`
 })
 export class StatisticsComponent implements OnInit {
     items: Statistic[];
+    public bottomBarShow = true;
+    pieSource: {Brand: string, Amount: number}[] = [
+        { Brand: 'Audi', Amount: 10 },
+        { Brand: 'Mercedes', Amount: 76 },
+        { Brand: 'Fiat', Amount: 60 },
+        { Brand: 'BMW', Amount: 24 },
+        { Brand: 'Crysler', Amount: 40 }
+];
+
 
     // This pattern makes use of Angular’s dependency injection implementation to inject an instance of the FriendService service into this class.
     // Angular knows about this service because it is included in your app’s main NgModule, defined in app.module.ts.
