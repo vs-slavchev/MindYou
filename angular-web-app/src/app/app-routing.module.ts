e76a38a1a89a718cd54370f46894a727fa0807c2
Iana Florea <i.florea@student.fontys.nl>
2018-11-27 16:09:33 +0100

    Http Request works

--- angular-web-app/src/app/app-routing.module.ts
+++ angular-web-app/src/app/app-routing.module.ts
@@ -7,15 +7,19 @@ import {ContactComponent} from "./contact/contact.component";
 import {UserProfileComponent} from "./user-profile/user-profile.component";
 import {UserTimelineComponent} from "./user-timeline/user-timeline.component";
 import {TestComponent} from "./test/test.component";
+import{GetRequestsComponent} from "./get-requests/get-requests.component";
+import {HttpTestComponent} from "./http-test/http-test.component";
 
 const routes:Routes=[
-    {path:'home', component: HomeComponent},
-    {path:'statistics', component:StatisticsComponent},
-    {path:'contact', component:ContactComponent},
-    {path:'', component:HomeComponent},
-    {path:'profile', component:UserProfileComponent},
-    {path:'timeline', component:UserTimelineComponent},
-    {path:'test', component:TestComponent}
+     {path:'home', component: HomeComponent},
+     {path:'statistics', component:StatisticsComponent},
+     {path:'contact', component:ContactComponent},
+     {path:'', component:HomeComponent},
+     {path:'profile', component:UserProfileComponent},
+     //{path:'timeline', component:UserTimelineComponent},
+     {path:'test', component:TestComponent},
+     {path:'get', component: GetRequestsComponent},
+    {path:'testcp',component: HttpTestComponent}
     ];
 @NgModule({
   declarations: [],
