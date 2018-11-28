f24101ec6addbfc5c13205ef868920b5c5251102
Juraj Korbacka <jkorbacka@gmail.com>
2018-11-28 21:19:23 +0100

    clean up comments
    Former-commit-id: bcb455773e48a9b70a4c55a5efeaac8cb63aa83f

--- ns-angular-mobile/src/app/home/statistic/statistic.service.ts
+++ ns-angular-mobile/src/app/home/statistic/statistic.service.ts
@@ -35,47 +35,6 @@ export class StatisticService {
             );
     }
 
-    // createAccount(): Observable<any> {
-    //     return this.http.post(`${AppSettings.API_URL}/users/create`, {"id": AppSettings.TOKEN},
-    //         this.addAuthToken());
-    // }
-
-    // getItemNo404<Data>(activityBlueprintId: number): Observable<Item> {
-    //     const url = `${this.activitiesUrl}/${activityBlueprintId}`;
-    //     return this.http.get<Item[]>(url)
-    //         .pipe(
-    //             map(items => items[0]), // returns a {0|1} element array
-    //             tap(h => {
-    //                 const outcome = h ? `fetched` : `did not find`;
-    //                 this.log(`${outcome} activity id=${activityBlueprintId}`);
-    //             }),
-    //             catchError(this.handleError<Item>(`getItem id=${activityBlueprintId}`))
-    //         );
-    // }
-    //
-    // getItem(activityBlueprintId: number): Observable<Item> {
-    //     const url = `${this.activitiesUrl}/${activityBlueprintId}`;
-    //     return this.http.get<Item>(url).pipe(
-    //         tap(_ => this.log(`fetched item id=${activityBlueprintId}`)),
-    //         catchError(this.handleError<Item>(`getItem id=${activityBlueprintId}`))
-    //     );
-    // }
-    //
-    // stopActivity(activityBlueprintId: number): Observable<Item> {
-    //     const url = `${this.activitiesUrl}/${AppSettings.TOKEN}/stop`;
-    //     return this.http.get<Item>(url).pipe(
-    //         tap(_ => this.log(`stopped activity id=${activityBlueprintId}`)),
-    //         catchError(this.handleError<Item>(`stopActivity id=${activityBlueprintId}`))
-    //     );
-    // }
-    //
-    // startActivity (activity: any): Observable<any> {
-    //     return this.http.post<any>(this.activitiesUrlStart, activity, httpOptions).pipe(
-    //         tap((activity: any) => this.log(`started activity w/ id=${activity.activity_id}`)),
-    //         catchError(this.handleError<any>('startActivity'))
-    //     );
-    // }
-
     private log(message: string) {
         console.log(`ActivityService: ${message}`);
         // this.messageService.add(`ActivityService: ${message}`);
--- ns-angular-mobile/src/app/home/statistic/statistics.component.ts
+++ ns-angular-mobile/src/app/home/statistic/statistics.component.ts
@@ -18,28 +18,6 @@ import { Label } from "tns-core-modules/ui/label";
 export class StatisticsComponent implements OnInit {
     statistics: Statistic[];
     public bottomBarShow = true;
-    // pieSource: {activity_name: string, hours: number}[] = [
-    //     { activity_name: 'Cleaning', hours: 0.33 },
-    //     { activity_name: 'Commuting', hours: 1.66 },
-    //     { activity_name: 'Cooking', hours: 1.33 },
-    //     { activity_name: 'Eating', hours: 1.25 },
-    //     { activity_name: 'Gym', hours: 2.00 },
-    //     { activity_name: 'Music', hours: 0.75 },
-    //     { activity_name: 'Reading', hours: 1.00 },
-    //     { activity_name: 'Studying', hours: 5.50 },
-    //     { activity_name: 'Working', hours: 8.00 }
-    // ];
-
-    // pieSource: { activity_name: string, hours: number }[] = [{
-    //     "activity_name": "cooking",
-    //     "hours": 0
-    // }, {
-    //     "activity_name": "gym",
-    //     "hours": 0.01
-    // }, {
-    //     "activity_name": "swimming",
-    //     "hours": 0.03
-    // }];
 
     pieSource: Statistic[];
 
@@ -50,12 +28,8 @@ export class StatisticsComponent implements OnInit {
     ngOnInit(): void {
         this.statisticService.getStatistics().subscribe((statistics) => {
             this.statistics = statistics;
-            // this.statistics.forEach(function (statistic: Statistic) {
-            //     pieSource.append({"activity_name": statistic.a, "hours": 0})
-            // });
-
             this.pieSource = statistics;
-            console.log(statistics);
+            // console.log(statistics);
         });
         // this.statistics = this.statisticService.getStatistics();
     }
