bf857fd55a56e2810ddc55ef7d7bfe3c0c2ec1f7
Juraj Korbacka <jkorbacka@gmail.com>
2018-11-30 13:21:36 +0100

    remove statistics detail component

--- ns-angular-mobile/src/app/home/home.module.ts
+++ ns-angular-mobile/src/app/home/home.module.ts
@@ -1,6 +1,5 @@
 import {NgModule, NO_ERRORS_SCHEMA} from '@angular/core';
 import {ItemDetailComponent} from "~/app/home/item/item-detail.component";
-import {StatisticDetailComponent} from "~/app/home/statistic/statistic-detail.component";
 import {StatisticsComponent} from "~/app/home/statistic/statistics.component";
 import {FriendsComponent} from "~/app/home/friend/friends.component";
 import {FriendDetailComponent} from "~/app/home/friend/friend-detail.component";
@@ -20,7 +19,6 @@ import {NativeScriptUIChartModule} from "nativescript-ui-chart/angular";
         ItemsComponent,
         ItemDetailComponent,
         StatisticsComponent,
-        StatisticDetailComponent,
         FriendsComponent,
         FriendDetailComponent,
         BottomBarComponent,
